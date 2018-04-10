b684f0f33147c0ba534476898f86d18337aafaea
Ihor <skliar.ihor@gmail.com>
2018-04-10 11:35:36 +0300

    Added error handling

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+function isInt(n){
+    return Number(n) === n && n % 1 === 0;
+}
+
 module.exports = function isPrime(n) {
+    if (!isInt(n)) {
+        throw new Error('Only integer numbers is accepted');
+    }
+
+    if (n < 0) {
+        throw new Error('Only positive numbers is accepted');
+    }
+
     var re = /^.?$|^(..+?)\1+$/;
-    return !re.test(Array(n+1).join('1'));
+    return !re.test(Array(n + 1).join('1'));
 };
